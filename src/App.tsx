1f0dfbb4b269cf06b5e6816b37a361169db473a1
Igor.Uss <iigor.uss@gmail.com>
2021-10-08 23:07:48 +0300

    1.func(inc,det,min,max,set)_useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
+import {DisplayControl} from "./components/counter/displayControl/DisplayControl";
+import {Setting} from "./components/counter/setting/Setting";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    let [state, setState] = useState([{
+        max: 0,
+        min: 0,
+        displayValue:0
+
+    }])
+
+    let [settingState, setSettingState] = useState([{
+        minSettingState: 0,
+        maxSettingState: 0
+    }])
+
+
+    const settingMinHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        settingState[0].minSettingState = Number(e.currentTarget.value)
+    }
+    const settingMaxHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        settingState[0].maxSettingState = Number(e.currentTarget.value)
+    }
+    const set = () => {
+        state[0].min = settingState[0].minSettingState
+        state[0].max = settingState[0].maxSettingState
+        state[0].displayValue=state[0].min
+
+
+        setSettingState(settingState)
+        setState([...state])
+
+    }
+
+
+    const inc = () => {
+
+
+       if (state[0].min < state[0].max+1) {state[0].displayValue=state[0].min
+           state[0].min++;} else return
+        setState([...state])
+    }
+
+
+    return (
+        <div className="App">
+            <div>
+
+
+                <DisplayControl
+                    inc={inc}
+                    min={state[0].min}
+                    max={state[0].max}
+                    displayValue={state[0].displayValue}
+
+
+                />
+                <Setting
+                    settingMinHandler={settingMinHandler}
+                    settingMaxHandler={settingMaxHandler}
+                    set={set}
+                                   />
+
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
